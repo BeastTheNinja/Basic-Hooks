df6bc3af40efca6d8458af6bf4d00387e6e59bdc
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-01-12 10:51:19 +0100

    lavet en greeting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Counter } from "./Components/counter/Counter";
+import { Greeting } from "./Components/greeting/Greeting";
 
 function App() {
 
 
-return (
- <Counter />
-)
+    return (
+
+        <>
+            <Counter />
+
+            <Greeting />
+        </>
+    )
 }
 
 export default App
---
+++ src/Components/greeting/Greeting.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export const Greeting = () => {
+    const [name, setName] = useState<string>("");
+
+    return (
+        <section
+            className="
+            min-h-screen 
+            flex 
+            items-center 
+            justify-center
+            bg-gray-100">
+            <div
+                className="
+                bg-white 
+                p-6 
+                rounded-xl 
+                shadow-md 
+                w-full 
+                max-w-md">
+                <p
+                    className="
+                    text-xl 
+                    font-semibold 
+                    text-gray-800 
+                    mb-4">
+                    Send en hilsen til{" "}
+                    <span
+                        className="
+                    text-indigo-600">
+                        {name || "…"}
+                    </span>
+                </p>
+
+                <input
+                    type="text"
+                    placeholder="Indtast navn"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                    className="
+                        w-full
+                        px-4
+                        py-2
+                        border
+                        border-gray-300
+                        rounded-lg
+                        focus:outline-none
+                        focus:ring-2
+                        focus:ring-indigo-500
+                    "
+                />
+            </div>
+        </section>
+    );
+};
