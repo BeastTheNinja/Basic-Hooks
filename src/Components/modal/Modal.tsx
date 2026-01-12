00bfad477ba3242c922c559c3899083d23c9a9cd
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-01-12 12:54:35 +0100

    update

--- src/Components/modal/Modal.tsx
+++ src/Components/modal/Modal.tsx
@@ -7,7 +7,7 @@ interface ModalProps {
 
 export const Modal = ({ children, onClose }: ModalProps) => {
     return (
-        <div className="bg-white rounded-lg shadow-lg p-6 w-96 relative content-center">
+        <div className="bg-white rounded-lg shadow-lg p-6 w-96 relative">
             <button
                 onClick={onClose}
                 className="absolute top-2 right-2 text-gray-500 hover:text-black text-xl"
